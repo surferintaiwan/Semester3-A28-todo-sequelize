bd3586350de200ba9835731d032ec00d64598d17
Shawn <pan.conan@gmail.com>
2019-12-17 13:31:28 +0800

    add home and user routers

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-    console.log(req.body)
+
 })
 
 
@@ -18,11 +18,38 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
+    const {name, email, password, password2} = req.body
+    User.findOne({where:{email:email}})
+        .then(user => {
+            if (user) {
+                console.log('User already exists')
+                res.render('register', {
+                    name,
+                    email,
+                    password,
+                    password2
+                })    
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                })
+                newUser
+                    .save()
+                    .then(user => {
+                        res.redirect('/')
+                    })
+                    .catch(err => console.log(err))
+            }
+        })
+    /*
     User.create({
         name:req.body.name,
         email: req.body.email,
         password: req.body.password
     }).then(user => {res.redirect('/')})
+    */
 })
 
 
